33999c4384f96e49ac7d29a6008756df58cf64f1
e0 <shan.huang@me.com>
2018-08-26 15:07:45 +0200

    Add foldDyn example

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -16,9 +16,9 @@ import Static
 frontend :: (StaticWidget x (), Widget x ())
 frontend = (head', body)
   where
-    head' =  do
-     el "title" $ text "Obelisk Minimal Example"
-     elAttr "link" attrsCSS blank
+    head' = do
+      el "title" $ text "Obelisk Minimal Example"
+      elAttr "link" attrsCSS blank
     body = do
       text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
@@ -33,14 +33,20 @@ frontend = (head', body)
       el "h1" $ text "A link to DuckDuckGo in a new tab"
       elAttr "a" attrsLink $ text "DuckDuckGo"
       elClass "h2" "sub-title" $ text "This is a sub title"
-      rec
-        dynBool <- toggle False evClick
-        let dynAttrs = attrsColorToggle <$> dynBool
-        elDynAttr "h1" dynAttrs $ text "Changing color"
-        evClick <- button "Change Color"
+      rec dynBool <- toggle False evClick
+          let dynAttrs = attrsColorToggle <$> dynBool
+          elDynAttr "h1" dynAttrs $ text "Changing color"
+          evClick <- button "Change Color"
       return ()
       el "br" blank
+      el "h2" $ text "Counter"
       button "Click me" >>= count >>= display
+      el "br" blank
+      rec el "h2" $ text "Counter as a fold"
+          numbs <- foldDyn (+) (0 :: Int) (1 <$ evIncr)
+          evIncr <- button "Increment"
+          el "span" $ display numbs
+      return ()
 
 attrsCSS :: Map.Map T.Text T.Text
 attrsCSS = ("href" =: static @"style.css") <> ("rel" =: "stylesheet")
@@ -50,6 +56,6 @@ attrsLink = ("target" =: "_blank") <> ("href" =: "http://duckduckgo.com")
 
 attrsColorToggle :: Bool -> Map.Map T.Text T.Text
 attrsColorToggle b = "style" =: ("color: " <> color b)
-  where 
+  where
     color True = "red"
-    color _    = "green"
+    color _ = "green"
